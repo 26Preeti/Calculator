086e655c810a009585b04fceda25110de2381603
Preeti <preetitrikha26@yahoo.com>
2022-05-03 05:11:00 +0530

    OWN CODE for each operation

--- index.js
+++ index.js
@@ -1,27 +1,161 @@
 let screen=document.getElementById("screen");
 let screenValue="";
-
+var res=0;
 buttons=document.querySelectorAll("button");
 for(b of buttons){
     b.addEventListener('click', (e)=>{
         buttonText = e.target.innerText;
-       
-        if(buttonText=="X"){
-            buttonText="*";
+        
+     
+        if(buttonText=="X" || buttonText=="+" || buttonText=="-"|| buttonText=="/"|| buttonText=="%"  || buttonText=="< <" || buttonText==">>" || buttonText=="&" || buttonText=="|" || buttonText=="^" ||buttonText=="~"){
+            if(buttonText=="X"){
+              buttonText="*";
+            }
+            if(buttonText=="< <"){
+                buttonText="<<";
+            }   
+            if((/[0-9]*[+][0-9]*/).test(screenValue)){
+              const a=screenValue.split("+");
+              res=Number(a[0])+Number(a[1]);
+              screenValue=res+buttonText;
+              screen.value=screenValue;
+            }
+            else if((/[0-9]*[-][0-9]*/).test(screenValue)){
+                const a=screenValue.split("-");
+                res=Number(a[0])-Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+              }
+            else if((/[0-9]*[/][0-9]*/).test(screenValue)){
+                const a=screenValue.split("/");
+                res=Number(a[0])/Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*[%][0-9]*/).test(screenValue)){
+                const a=screenValue.split("%");
+                res=Number(a[0])%Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*[*][0-9]*/).test(screenValue)){
+                const a=screenValue.split("*");
+                res=Number(a[0])*Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['<<'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("<<");
+                res=Number(a[0])<<Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['>>'][0-9]*/).test(screenValue)){
+                const a=screenValue.split(">>");
+                res=Number(a[0])>>Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['>>>'][0-9]*/).test(screenValue)){
+                const a=screenValue.split(">>>");
+                res=Number(a[0])>>>Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['&'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("&");
+                res=Number(a[0])&Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['|'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("|");
+                res=Number(a[0])|Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/[0-9]*['^'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("^");
+                res=Number(a[0])^Number(a[1]);
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else if((/['~'][0-9]*/).test(screenValue)){
+                
+                res=~(Number(screenValue));
+                screenValue=res+buttonText;
+                screen.value=screenValue;
+            }
+            else{
             screenValue+=buttonText;
             screen.value=screenValue;
-        }
-        else if(buttonText=="< <"){
-            buttonText="<<";
-            screenValue+=buttonText;
-            screen.value=screenValue;
+            }
         }
         else if(buttonText=="C"){
+      
             screenValue="";
             screen.value=screenValue;
         }
         else if(buttonText=="="){
-            screenValue=eval(screenValue);
+            if((/[0-9]*[+][0-9]*/).test(screenValue)){
+                const a=screenValue.split("+");
+                res=Number(a[0])+Number(a[1]);
+              }
+              else if((/[0-9]*[-][0-9]*/).test(screenValue)){
+                  const a=screenValue.split("-");
+                  res=Number(a[0])-Number(a[1]);
+                }
+              else if((/[0-9]*[/][0-9]*/).test(screenValue)){
+                  const a=screenValue.split("/");
+                  res=Number(a[0])/Number(a[1]);
+
+              }
+              else if((/[0-9]*[%][0-9]*/).test(screenValue)){
+                const a=screenValue.split("%");
+                res=Number(a[0])%Number(a[1]);
+
+            }
+              else if((/[0-9]*[*][0-9]*/).test(screenValue)){
+                  const a=screenValue.split("*");
+                  res=Number(a[0])*Number(a[1]);
+    
+              }
+              else if((/[0-9]*['<<'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("<<");
+                res=Number(a[0])<<Number(a[1]);
+                
+            }
+            else if((/[0-9]*['>>'][0-9]*/).test(screenValue)){
+                const a=screenValue.split(">>");
+                res=Number(a[0])>>Number(a[1]);
+              
+            }
+            else if((/[0-9]*['>>>'][0-9]*/).test(screenValue)){
+                const a=screenValue.split(">>>");
+                res=Number(a[0])>>>Number(a[1]);
+                
+            }
+            else if((/[0-9]*['&'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("&");
+                res=Number(a[0])&Number(a[1]);
+               
+            }
+            else if((/[0-9]*['|'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("|");
+                res=Number(a[0])|Number(a[1]);
+                
+            }
+            else if((/[0-9]*['^'][0-9]*/).test(screenValue)){
+                const a=screenValue.split("^");
+                res=Number(a[0])^Number(a[1]);
+                
+            }
+            else if((/['~'][0-9]*/).test(screenValue)){
+                let a=Number(screenValue[1]);
+                res=(~a);
+              
+            }
+            screenValue=res;
             screen.value=screenValue;
         }    
         else if(buttonText=="x"){
